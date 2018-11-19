47b1443b15821f0d506be33b3f7f273359f15a7d
ashaunthomas <ashaunthomas@siu.edu>
2018-11-18 21:22:49 -0700

    Added 1.1 function description

--- Ch1/1.1.js
+++ Ch1/1.1.js
@@ -9,6 +9,15 @@ let testCases = [
     '1234'
 ];
 
+/**
+ * This function checks to see if a string is unique by using
+ * the built-in Set object. This implementation takes advantage
+ * of the fact that the Set object doesn't allow duplicates by
+ * comparing the size of the array as a set. All duplicates would
+ * be removed in the set, causing it's size to be smaller than the
+ * length of an array representing the string
+ * @param string String of characters to analyze
+ */
 function isUnique(string) {
     let inputStringArr = string.toLowerCase().split('');
     let inputSet = new Set(inputStringArr);
