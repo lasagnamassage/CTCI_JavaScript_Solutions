d82b4877635186ad2c7606bf223b4b5501f235a2
ashaunthomas <ashaunthomas@siu.edu>
2018-11-22 22:59:59 -0700

    1.4 boilerplate

---
+++ Ch1/1.4.js
@@ -0,0 +1,24 @@
+/**
+ * Given a string, check if its a permutation of
+ * a palindrome. The palindrome doesn't have to be
+ * a valid English word
+ * @author Ashaun Thomas
+ */
+
+ var testCases = [
+     "tact coa"
+ ]
+
+
+ function isPalindromePermutation(string) {
+
+ }
+
+
+ function main() {
+     for (let i = 0; i < testCases.length; i++) {
+         console.log(isPalindromePermutation(testCases[i]));
+     }
+ }
+
+ main();
