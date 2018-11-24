c4ef271e5671c9c2f5bc62461c6a372f8d725437
ashaunthomas <ashaunthomas@siu.edu>
2018-11-23 18:25:49 -0700

    1.4 Complete.

--- Ch1/1.4.js
+++ Ch1/1.4.js
@@ -6,14 +6,46 @@
  */
 
  var testCases = [
-     "tact coa"
+     "tact coa",
+     "hannah",
+     "dksladka dksld kasldk sal",
+     "racecar", 
+     "Repaper"
  ]
 
 
- function isPalindromePermutation(string) {
+ /**
+  * If string is even, the string must have two instances of any character in the string to
+  * be a palindrome permutation. 
+  * 
+  * If string is odd, the string must have two instances of any character in the string except for one character,
+  * the middle one...
+  *  
+  */
 
+
+// tactcoa
+// aacctto
+
+ function isPalindromePermutation(string) {
+    let sanitizedStr = string.replace(/\s/g,'').toLowerCase().split('').join('');
+    let sanitizedSet = new Set(sanitizedStr);
+    let expectedLength;
+    if (isEven(sanitizedStr)) {
+        expectedLength = sanitizedStr.length/2;
+    }
+    else {
+        expectedLength = Math.floor(sanitizedStr.length/2 + 1);
+    }
+    // console.log("String: " + sanitizedStr.length);
+    // console.log("Set: " + sanitizedSet.size);
+    // console.log("Expected length: " + expectedLength);
+    return sanitizedSet.size === expectedLength ? true : false;
  }
 
+ function isEven(string) {
+     return string.length % 2 == 0;
+ }
 
  function main() {
      for (let i = 0; i < testCases.length; i++) {
