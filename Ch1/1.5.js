9619b1b5f5c197a3363df500818af032715544bd
ashaunthomas <ashaunthomas@siu.edu>
2018-11-23 18:33:18 -0700

    1.5 boilerplate.

---
+++ Ch1/1.5.js
@@ -0,0 +1,21 @@
+/**
+ * Given two strings, write a function to hecks if they are
+ * one or zero edits away, given the following possible edits:
+ * 1. Insert a character
+ * 2. Remove a character
+ * 3. Replace a character
+ * @author Ashaun Thomas
+ */
+
+
+ testCases = [];
+
+ function isEditAway(string) {
+
+ }
+
+ function main() {
+    for( let i = 0; i < testCases.length; i++) {
+        isEditAway(testCases[i]);
+    }
+ }
