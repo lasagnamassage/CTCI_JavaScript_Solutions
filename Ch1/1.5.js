7b4bd8d26b2e7216b4c48cc649605fb8aea9b10b
ashaunthomas <ashaunthomas@siu.edu>
2018-11-24 17:07:43 -0700

    1.5 Complete.

--- Ch1/1.5.js
+++ Ch1/1.5.js
@@ -4,13 +4,13 @@
  * 1. Insert a character
  * 2. Remove a character
  * 3. Replace a character
- * TODO: Finish functions to test 3 edits
  * @author Ashaun Thomas
  */
 
  testCases = [
-     ["pale", "ple"],
-     ["pale", "ppl"]
+     ["pale", "ple"], //true
+     ["pale", "ppl"], //false
+     ["pale", "pwle"] //true
  ];
 
  function isEditAway(inputs) {
@@ -23,51 +23,60 @@
     if (Math.abs(((inputs[0].length - inputs[1].length)) > 1)) {
         return false;
     }
+    
+    let map1 = hashify(inputs[0]);
+    let map2 = hashify(inputs[1]);
     if (inputs[0].length === inputs[1].length) {
-        // We can only run a replace edit here 
-    }
-    else {
-        // We can only run add or remove edits here
-        let map1 = hashify(inputs[0]);
-        let map2 = hashify(inputs[1]); 
-        if (editOneCheck(map1,map2)) {
+        // We can only run a replace edit in this case
+        if (editThreeCheck(map1, map2)) {
             return true;
         }
-        
+    }
+    else {
+        // We can only run add or remove edits in this case
+        if (editOneOrTwoCheck(map1,map2)) {
+            return true;
+        }
+
         return false;
     }
  }
 
 
 /**
- * Checks for edit 1 possibility: character insertion
- * @param  string1 String to check for character insertion
- * @param  string2 String to check for character insertion
+ * Checks for edit 1 or two possibility: character insertion
+ * by popping matching Map pair occurances until left
+ * with 1 set in the largest map in the event of 'true',
+ * or some value not equal to one in the event of 'false'
+ * @param  map1 String to check for character insertion
+ * @param  map2 String to check for character insertion
  */
-function editOneCheck(map1, map2) {
-    let smallest = (map1.size < map2.size) ? map1 : map2;
-    let largest = (smallest === map1) ? map2 : map1;
-    // Needs to pop matching pairs from largest
-    for (let [key, value] of largest) {
-        if (largest.has(key) && smallest.has(key)) {
-            smallest.set(key, value - 1);
-            largest.set(key, value - 1);
-            if (smallest.get(key) == 0) {
-                smallest.delete(key);
-            }
-            if (largest.get(key) == 0) {
-                largest.delete(key);
-            }
-        }
-    }
-    if (smallest.size === 0 && largest.size === 1) {
-        return true;
+function editOneOrTwoCheck(map1, map2) {
+    let remainderMaps = extractLikeTerms(map1, map2);
+    if ((remainderMaps[0].size === 0 && remainderMaps[1].size === 1) ||
+        (remainderMaps[1].size === 0 && remainderMaps[0].size === 1)) {
+            return true;
     }
     return false;
 }   
 
 /**
- * Turns a string into a JS-native hashmap implementation.
+ * Checks for edit 3 possibility: replace a character
+ * @param {*} map1 String to check for character insertion
+ * @param {*} map2 String to check for character insertion
+ */
+function editThreeCheck(map1, map2) {
+    let remainderMaps = extractLikeTerms(map1, map2);
+    if (remainderMaps[0].size === remainderMaps[1].size) {
+        return true;
+    }
+    return false;
+
+}
+
+/**
+ * Helper function that turns a string into a JS-native 
+ * hashmap implementation.
  * @param string String to be hashified 
  */
 function hashify(string) {
@@ -78,6 +87,29 @@ function hashify(string) {
         map.set(key,value);
     }
     return map;
+}
+
+/**
+ * Helper function that removes like entries and occurences
+ * from two maps.
+ * @param map1 Map to operate on
+ * @param map2 Map to operate on
+ * @return Array of the modified map1 and map2
+ */
+function extractLikeTerms(map1, map2) {
+    for (let [key, value] of map1) {
+        if (map1.has(key) && map2.has(key)) {
+            map2.set(key, value - 1);
+            map1.set(key, value - 1);
+            if (map2.get(key) == 0) {
+                map2.delete(key);
+            }
+            if (map1.get(key) == 0) {
+                map1.delete(key);
+            }
+        }
+    }
+    return [map1, map2];
 }
 
 /**
