157df33368f2908961f379c9eedaf256321bcc34
ashaunthomas <ashaunthomas@siu.edu>
2018-11-24 17:14:35 -0700

    1.6 Boilerplate.

---
+++ Ch1/1.6.js
@@ -0,0 +1,25 @@
+/**
+ * Implement a method to perform string compression
+ * using the counts of repeated characters.
+ * For example, "aabccccaaa" would become "a2b1c4a3".
+ * If the compressed string would not become smaller than
+ * the original string, your method should return the original
+ * string. Assume the string only has uppercase and lowercase
+ * letters (a-z)
+ * @author Ashaun Thomas
+ */
+
+ var testCases = [];
+
+ function stringCompressor(string) {
+
+ }
+
+ /**
+  * Runs test cases
+  */
+ function main() {
+     for (let i = 0; i < testCases.length; i++) {
+         console.log(stringCompressor(testCases[i]));
+     }
+ }
