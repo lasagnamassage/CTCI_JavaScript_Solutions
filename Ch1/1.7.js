346c75aa557d4a334d6af6971fd55286e772aa35
ashaunthomas <ashaunthomas@siu.edu>
2018-11-28 19:36:05 -0700

    1.6 Boilerplate

---
+++ Ch1/1.7.js
@@ -0,0 +1,21 @@
+/**
+ * Given an image represented as a square matric (N by N),
+ * where each pixel in the image is 4 bytes.
+ * Write a method to rotate the image by 90 degrees.
+ * Can you do this "in place"?
+ * @author Ashaun Thomas
+ */
+
+let testCases = [
+
+];
+function rotate90(matrix) {}
+
+/**
+ * Runs test cases
+ */
+function main() {
+    for (let i = 0; i < testCases.length; i++) {
+        console.log(rotate90(testCases[i]));
+    }
+}
